5c7801e632158a20e50904742bbe168a0de956ff
김인성 <78129173+jk090909@users.noreply.github.com>
2021-11-03 20:25:06 +0900

    Add files via upload

---
+++ js/step1/js/1_1_check.js
@@ -0,0 +1,17 @@
+function runAlert(){
+    alert('손ㅈㄴ아파');
+}
+// !경고창 띄움
+
+function runWrite(){
+    document.write('hi' + '<br>' + 'javascript');
+}
+// !문서로 나옴
+
+function runLog(){
+    console.log('hi' + '<br>' + 'javascript');
+}
+// !콘솔창에 나옴
+
+
+// ? 우리가 만든 함수인가?  함수 = 기능. 함수 = 이미 만들어진것(라이브러리), 내가 만드는 것(임의).
