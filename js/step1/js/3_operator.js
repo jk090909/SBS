037e46c0a14323dda3560f6a58bbd5ff049180f2
김인성 <78129173+jk090909@users.noreply.github.com>
2021-11-09 19:04:09 +0900

    Add files via upload

--- js/step1/js/3_operator.js
+++ js/step1/js/3_operator.js
@@ -53,8 +53,81 @@ function operatorEx(){
 // *case.4 연산자가 뒤로 올 경우.
 function numberPlusAfter(){
     var number = 10; //? number 라는 변수의 선언 및 10으로 초기화.
-    alert(number++); //? 10 
-    alert(number++); //? 11
-    alert(number++); //? 12
-    alert(number++); //? 13
+    alert(number++); //? 10을 출력후 ++증가. 다시 number로-
+    alert(number++); //? 11을 출력후 ++증가. 다시 number로-
+    alert(number++); //? 12를 출력후 ++증가. 다시 number로 = 최종 13.
+    console.log(number);
 }
+
+// *case.5 연산자가 앞으로 올 경우/
+function numberPlusBefore(){
+    var number = 10;
+    alert(++number); //? 10에 1을 더해서 (11) 출력후, 다시 number 로 -
+    alert(++number); //? 11에 1을 더해서 (12) 출력후, 다시 number 로 -
+    alert(++number); //? 12에 1을 더해서 (13) 출력후, 다시 number 로 -
+}
+// todo. 증감연산자복합사용
+function numberPlusTodo(){
+    var number = 10;
+    alert(number++); //? 출력: 10 / 이후 : 11
+    alert(--number); //? 출력: 10 / 이후 : 10
+    alert(++number); //? 출력: 11 / 이후 : 11
+    alert(number--); //? 출력: 11 / 이후 : 10
+    console.log(number); //? 최종 : 10
+}
+
+// *case.6 숫자 10이 담긴 변수a 와 숫자 20이 담긴 변수b의 크기를 비교하여 결과출력.
+function compareOperator1(){
+    var a = 10, b = 20;
+    var result = a > b;
+    console.log("10 > 20 = " + result);
+}
+// *case.7 숫자 10이 담긴 변수a 와 숫자 20이 담긴 변수b가 같은지 비교및 결과출력.
+function compareOperator2(){
+    var a = 10;
+    var b = 20;
+    var result = a == b;
+    console.log("10 = 20 ? " + result);
+}
+// todo. 숫자 10이 담긴 변수a와 숫자 20이 담긴 변수b가 "다른지" 비교하여 결과출력.
+function compareOperator3(){
+    var a = 10;
+    var b = 20;
+    var result = a!=b;
+    console.log("10 != 20 ? " + result);
+}
+
+/* 
+    * case.8 a변수에는 '여자, b변수에는 '웹디자이너' 라는 값이 저장되어 있다.
+    * 담겨있는 두 값이 모두 맞는지 result 변수에 저장 후 출력.
+*/
+function logicalOperator1(){
+    var a = '여자';
+    var b = '웹퍼블리셔';
+    var result = (a=='여자')&&(b=='웹디자이너');
+    console.log('result = ' + result);
+}
+/*
+    * case.9 a변수에는 '남자', b변수에는 '웹퍼블리셔' 라는 값이 저장되어 있다.
+    * 담겨있는 두 값중 하나라도 맞는지 result 변수에 저장 후 출력.
+*/
+function logicalOperator2(){
+    var a = '여자';
+    var b = '웹퍼블리셔';
+    var result = (a=='남자')||(b == '웹퍼블리셔');
+    console.log('result = ' + result);
+}
+// todo. 다음 코드의 결과를 예상한 다음 출력.
+function compareOperator4(){
+    console.log(30 > 20 > 10); //? 예상결과 : true / 출력결과 : false
+}
+
+// todo. 정상적인 결과가 출력되도록 코드 수정.
+function compareOperator4R(){
+    console.log(30 > 20 && 20 > 10);
+}
+/*
+    ! (30 > 20 > 10); 의 결과식은 거짓으로 판별
+    ? (30 > 20) > 10, true > 10, 1> 10, false.
+    *그래서 논리곱연산자로 좌,우측을 따로 비교 후 결과 값 산출.
+*/
